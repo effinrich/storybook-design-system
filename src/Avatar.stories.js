f684a3a2fddc21a2ffb0edc3477be0f47f88bb4f
Rich <rich@freebirdrides.com>
2021-02-21 10:41:57 -0800

    add and build storybook

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,61 +1,73 @@
-import React from "react";
+import React from 'react'
 
-import { Avatar } from "./Avatar";
+import { Avatar } from './Avatar'
 
 export default {
-  title: "Design System/Avatar",
-  component: Avatar,
-};
+  title: 'Design System/Avatar',
+  component: Avatar
+}
 
-export const Standard = (args) => <Avatar {...args} />;
+// START New story using controls
+const Template = args => <Avatar {...args} />
+
+export const Controls = Template.bind({})
+Controls.args = {
+  loading: false,
+  size: 'tiny',
+  username: 'Rich Tillman',
+  src: 'https://avatars2.githubusercontent.com/u/263385'
+}
+// END New story using controls
+
+export const Standard = args => <Avatar {...args} />
 Standard.args = {
-  size: "large",
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
-};
+  size: 'large',
+  username: 'Rich Tillman',
+  src: 'https://avatars.githubusercontent.com/u/183598'
+}
 
-export const Sizes = (args) => (
+export const Sizes = args => (
   <div>
     <Avatar {...args} size="large" />
     <Avatar {...args} size="medium" />
     <Avatar {...args} size="small" />
     <Avatar {...args} size="tiny" />
   </div>
-);
+)
 Sizes.args = {
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
-};
+  username: 'Rich Tillman',
+  src: 'https://avatars.githubusercontent.com/u/183598'
+}
 
-export const Initials = (args) => (
+export const Initials = args => (
   <div>
-    <Avatar username="Tom Coleman" />
-    <Avatar username="Dominic Nguyen" />
+    <Avatar username="Rich Tillman" />
+    <Avatar username="Rich Tillman" />
     <Avatar username="Kyle Suss" />
     <Avatar username="Michael Shilman" />
   </div>
-);
+)
 
-export const Loading = (args) => (
-    <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
-    </div>
-  );
-  Loading.args = {
-    loading: true,
-  };
+export const Loading = args => (
+  <div>
+    <Avatar {...args} size="large" />
+    <Avatar {...args} size="medium" />
+    <Avatar {...args} size="small" />
+    <Avatar {...args} size="tiny" />
+  </div>
+)
+Loading.args = {
+  loading: true
+}
 
-export const Large = (args) => (
+export const Large = args => (
   <div>
     <Avatar loading size="large" />
-    <Avatar size="large" username="Tom Coleman" />
+    <Avatar size="large" username="Rich Tillman" />
     <Avatar
       size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
+      username="Rich Tillman"
+      src="https://avatars.githubusercontent.com/u/183598"
     />
   </div>
-);
+)
--- src/Link.stories.js
+++ src/Link.stories.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
-import { action } from '@storybook/addon-actions';
+import React from 'react'
+import styled from 'styled-components'
+import { action } from '@storybook/addon-actions'
 
-import { Icon } from './Icon';
-import { Link } from './Link';
-import { StoryLinkWrapper } from './StoryLinkWrapper';
+import { Icon } from './Icon'
+import { Link } from './Link'
+import { StoryLinkWrapper } from './StoryLinkWrapper'
 
 const CustomLink = styled(Link)`
   && {
     color: red;
   }
-`;
+`
 
-const onLinkClick = action('onLinkClick');
+const onLinkClick = action('onLinkClick')
 
 export default {
   title: 'Design System/Link',
-  component: Link,
-};
+  component: Link
+}
 
-export const All = (args) => (
+export const All = args => (
   <div>
-    <Link href="https://learnstorybook.com">Default</Link>
+    <Link href="https://github.com/effinrich">Default</Link>
     <br />
-    <Link secondary href="https://learnstorybook.com">
+    <Link secondary href="https://github.com/effinrich">
       Secondary
     </Link>
     <br />
-    <Link tertiary href="https://learnstorybook.com">
+    <Link tertiary href="https://github.com/effinrich">
       tertiary
     </Link>
     <br />
-    <Link nochrome href="https://learnstorybook.com">
+    <Link nochrome href="https://github.com/effinrich">
       nochrome
     </Link>
     <br />
-    <Link href="https://learnstorybook.com">
+    <Link href="https://github.com/effinrich">
       <Icon icon="discord" aria-hidden />
       With icon in front
     </Link>
     <br />
-    <Link containsIcon href="https://learnstorybook.com" aria-label="Toggle side bar">
+    <Link
+      containsIcon
+      href="https://github.com/effinrich"
+      aria-label="Toggle side bar"
+    >
       <Icon icon="sidebar" aria-hidden />
     </Link>
     <br />
-    <Link containsIcon withArrow href="https://learnstorybook.com">
+    <Link containsIcon withArrow href="https://github.com/effinrich">
       With arrow behind
     </Link>
     <br />
     <span style={{ background: '#333' }}>
-      <Link inverse href="https://learnstorybook.com">
+      <Link inverse href="https://github.com/effinrich">
         Inverted colors
       </Link>
     </span>
@@ -60,12 +64,20 @@ export const All = (args) => (
       is actually a button
     </Link>
     <br />
-    <Link tertiary LinkWrapper={StoryLinkWrapper} href="http://storybook.js.org">
+    <Link
+      tertiary
+      LinkWrapper={StoryLinkWrapper}
+      href="http://storybook.js.org"
+    >
       has a LinkWrapper like GatsbyLink or NextLink
     </Link>
     <br />
-    <CustomLink tertiary LinkWrapper={StoryLinkWrapper} href="http://storybook.js.org">
+    <CustomLink
+      tertiary
+      LinkWrapper={StoryLinkWrapper}
+      href="http://storybook.js.org"
+    >
       has a LinkWrapper like GatsbyLink or NextLink with custom styling
     </CustomLink>
   </div>
-);
+)
